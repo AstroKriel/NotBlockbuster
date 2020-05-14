549d7a03cdbcdf9a2aafa1ca7782c1613d5e9477
necokriel <necokriel@gmail.com>
2020-05-14 20:42:53 +1000

    Add: minor comments

--- NotBlockbuster/Program.cs
+++ NotBlockbuster/Program.cs
@@ -16,6 +16,11 @@ namespace NotBlockbuster {
             // Initialise the array of members
             member_collec = new MemberCollection();
 
+
+            ////////////////////////////////////////////////////////////////////////
+            /// FOR TESTING FUNCTIONALITY
+            ////////////////////////////////////////////////////////////////////////
+
             // Create movie objects
             Movie bugs = new Movie("A Bug's Life", Movie.Genres.Animated, Movie.Classifications.General, "John Lasseter", "Dave Foley", 95, new DateTime(3 / 121998), 2);
             Movie avatar = new Movie("Avatar", Movie.Genres.Action, Movie.Classifications.MatureAccompanied, "Quentin Tarantino", "Leonardo DiCaprio", 165, new DateTime(24 / 01 / 2013), 11);
@@ -50,7 +55,7 @@ namespace NotBlockbuster {
             // Add members to the array
             member_collec.AddMember(neco);
             member_collec.AddMember(eve);
-
+            // Add movies to Neco's record
             int index;
             for (index = 0; index < 2; index++) { neco.RentMovie(billy); neco.ReturnMovie(billy); }
             for (index = 0; index < 5; index++) { neco.RentMovie(django); neco.ReturnMovie(django); }
@@ -64,32 +69,36 @@ namespace NotBlockbuster {
             for (index = 0; index < 20; index++) { neco.RentMovie(avatar); neco.ReturnMovie(avatar); }
             for (index = 0; index < 40; index++) { neco.RentMovie(banjo); neco.ReturnMovie(banjo); }
             for (index = 0; index < 44; index++) { neco.RentMovie(frozen); neco.ReturnMovie(frozen); }
+            // Add movies to Neco's currently renting list
+            neco.RentMovie(avatar);
+            neco.RentMovie(bugs);
+            neco.RentMovie(banjo);
+            neco.RentMovie(billy);
+            neco.RentMovie(django);
+            neco.RentMovie(hellboy);
+            neco.RentMovie(spiderman);
+            neco.RentMovie(jaws);
+            neco.RentMovie(pulp);
+            neco.RentMovie(frozen);
+            // Add movies to Eve's record
+            for (index = 0; index < 1; index++) { eve.RentMovie(django); eve.ReturnMovie(django); }
+            for (index = 0; index < 9; index++) { eve.RentMovie(pulp); eve.ReturnMovie(pulp); }
+            for (index = 0; index < 13; index++) { eve.RentMovie(bugs); eve.ReturnMovie(bugs); }
+            for (index = 0; index < 17; index++) { eve.RentMovie(jaws); eve.ReturnMovie(jaws); }
+            for (index = 0; index < 26; index++) { eve.RentMovie(lego); eve.ReturnMovie(lego); }
+            for (index = 0; index < 28; index++) { eve.RentMovie(hellboy); eve.ReturnMovie(hellboy); }
+            for (index = 0; index < 30; index++) { eve.RentMovie(avatar); eve.ReturnMovie(avatar); }
+            for (index = 0; index < 33; index++) { eve.RentMovie(banjo); eve.ReturnMovie(banjo); }
+            for (index = 0; index < 37; index++) { eve.RentMovie(totalrecall); eve.ReturnMovie(totalrecall); }
+            for (index = 0; index < 40; index++) { eve.RentMovie(frozen); eve.ReturnMovie(frozen); }
+            for (index = 0; index < 50; index++) { eve.RentMovie(billy); eve.ReturnMovie(billy); }
+            for (index = 0; index < 55; index++) { eve.RentMovie(spiderman); eve.ReturnMovie(spiderman); }
 
-            //neco.RentMovie(avatar);
-            //neco.RentMovie(bugs);
-            //neco.RentMovie(banjo);
-            //neco.RentMovie(billy);
-            //neco.RentMovie(django);
-            //neco.RentMovie(hellboy);
-            //neco.RentMovie(spiderman);
-            //neco.RentMovie(jaws);
-            //neco.RentMovie(pulp);
-            //neco.RentMovie(frozen);
 
-            //for (index = 0; index < 1; index++)  { eve.RentMovie(django);       eve.ReturnMovie(django); }
-            //for (index = 0; index < 9; index++)  { eve.RentMovie(pulp);         eve.ReturnMovie(pulp); }
-            //for (index = 0; index < 13; index++) { eve.RentMovie(bugs);         eve.ReturnMovie(bugs); }
-            //for (index = 0; index < 17; index++) { eve.RentMovie(jaws);         eve.ReturnMovie(jaws); }
-            //for (index = 0; index < 26; index++) { eve.RentMovie(lego);         eve.ReturnMovie(lego); }
-            //for (index = 0; index < 28; index++) { eve.RentMovie(hellboy);      eve.ReturnMovie(hellboy); }
-            //for (index = 0; index < 30; index++) { eve.RentMovie(avatar);       eve.ReturnMovie(avatar); }
-            //for (index = 0; index < 33; index++) { eve.RentMovie(banjo);        eve.ReturnMovie(banjo); }
-            //for (index = 0; index < 37; index++) { eve.RentMovie(totalrecall);  eve.ReturnMovie(totalrecall); }
-            //for (index = 0; index < 40; index++) { eve.RentMovie(frozen);       eve.ReturnMovie(frozen); }
-            //for (index = 0; index < 50; index++) { eve.RentMovie(billy);        eve.ReturnMovie(billy); }
-            //for (index = 0; index < 55; index++) { eve.RentMovie(spiderman);    eve.ReturnMovie(spiderman); }
-
-            // Start the Program
+            ////////////////////////////////////////////////////////////////////////
+            /// START OF THE PROGRAM
+            ////////////////////////////////////////////////////////////////////////
+            
             program_finished = false;
             while (!program_finished) {
                 Components.Menu.MainMenu();
